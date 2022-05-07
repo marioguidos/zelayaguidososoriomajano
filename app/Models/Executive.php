e80da5799959e5a5de4e4813beebc9c2203b2186
marioguidos <82772404+marioguidos@users.noreply.github.com>
2022-05-07 14:20:05 -0600

    migraciones y relaciones

--- app/Models/Executive.php
+++ app/Models/Executive.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Executive extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'dui',
+        'direction',
+        'category',
+        'user_id',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
--- database/migrations/2022_05_07_195411_create_clients_table.php
+++ database/migrations/2022_05_07_195411_create_clients_table.php
@@ -13,8 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
+
+        Schema::create('executives',function (Blueprint $table)
+        {
+            $table->id();
+            $table->string('dui')->unique();
+            $table->string('category');
+            $table->foreignId('user_id')->references('id')->on('users')
+            ->comment('Relacion con tabla usuarios');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->double('amount',0.2);
+            $table->string('direction');
+            $table->string('phoneNumber')->unique();
+            $table->foreignId('executive_id')->references('id')->on('executives')
+            ->comment('Relacion con tabla ejecutivos');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
